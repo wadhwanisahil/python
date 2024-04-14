5c5cae9c9371528f28b6e3298613a66a6ee09830
Sahil kumar <136256288+wadhwanisahil@users.noreply.github.com>
2024-04-14 10:32:33 -0700

    Add files via upload

---
+++ python/hello.py
@@ -0,0 +1,8 @@
+print("sahil kumar")
+age=90
+name='sahil '
+is_t=True
+print(is_t)
+print(age)
+print(name)
+
---
+++ python/input.py
@@ -0,0 +1,9 @@
+name=input("what is your name  ")
+print('hi  '+name)
+first= 'sahil'
+last= 'kumar'
+msg=f'{first} [{last}]'
+print(msg)
+find="course of python"
+print(len(find))
+print(find.upper())
